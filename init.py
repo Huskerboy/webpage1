84cad5daba097889f3633897edfa52db1392cb60
Huskerboy <jmeyer2534@gmail.com>
2017-03-10 16:57:18 -0600

    fixed the random draw card function and got it to output the card display.

--- init.py
+++ init.py
@@ -78,78 +78,91 @@ def _get_faces():
         {
             'name': 'Two',
             'rank': 2,
+            'symbol': 2,
             'weight': 2,
             'style_rank': 'rank-2'
         },
         {
             'name': 'Three',
             'rank': 3,
+            'symbol': 3,
             'weight': 3,
             'style_rank': 'rank-3'
         },
         {
             'name': 'Four',
             'rank': 4,
+            'symbol': 4,
             'weight': 4,
             'style_rank': 'rank-4'
         },
         {
             'name': 'Five',
             'rank': 5,
+            'symbol': 5,
             'weight': 5,
             'style_rank': 'rank-5'
         },
         {
             'name': 'Six',
             'rank': 6,
+            'symbol': 6,
             'weight': 6,
             'style_rank': 'rank-6'
         },
         {
             'name': 'Seven',
             'rank': 7,
+            'symbol': 7,
             'weight': 7,
             'style_rank': 'rank-7'
         },
         {
             'name': 'Eight',
             'rank': 8,
+            'symbol': 8,
             'weight': 8,
             'style_rank': 'rank-8'
         },
         {
             'name': 'Nine',
             'rank': 9,
+            'symbol': 9,
             'weight': 9,
             'style_rank': 'rank-9'
         },
         {
             'name': 'Ten',
             'rank': 10,
+            'symbol': 10,
             'weight': 10,
             'style_rank': 'rank-10'
         },
         {
             'name': 'Jack',
             'rank': 11,
+            'symbol': 'J',
             'weight': 10,
             'style_rank': 'rank-j'
         },
         {
             'name': 'Queen',
             'rank': 12,
+            'symbol': 'Q',
             'weight': 10,
             'style_rank': 'rank-q'
         },
         {
             'name': 'King',
             'rank': 13,
+            'symbol': 'K',
             'weight': 10,
             'style_rank': 'rank-k'
         },
         {
             'name': 'Ace',
             'rank': 14,
+            'symbol': 'A',
             'weight': 11,
             'style_rank': 'rank-a'
         },
