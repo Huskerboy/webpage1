8f1770c549fdd04f769ef81680122e996c31d7c4
Huskerboy <jmeyer2534@gmail.com>
2017-07-11 20:27:15 -0500

    Users can now create accounts, login, and logout.

--- init.py
+++ init.py
@@ -78,7 +78,7 @@ def login():
 def logout():
     logout_user()
     flash("You have been logged out.", "success")
-    return redirect(url_for('main'))
+    return redirect(url_for('index'))
 
 
 @app.route('/', methods=['GET', 'POST'])
